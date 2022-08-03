6204e0aee03bb5d4850ce2358cb60abf25724b0a
andrelucma <andrelucma@gmail.com>
2022-08-02 23:13:37 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import BlogMain from './components/BlogMain.vue'
+</script>
+
+<template>
+  <div>
+  <p>        
+    <a href="" target="_blank" class="header_button">Blog</a>
+    <a href="" target="_blank" class="header_button">Sobre</a>    
+    <a href="" target="_blank" class="header_button">Descrição</a>    
+  </p>    
+      <img src="/vite.svg" class="logo" alt="Vite logo" />    
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />    
+  </div>  
+  <BlogMain msg="André Marques" />  
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+.header_button {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+</style>
---
+++ src/components/BlogMain.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">Curtir {{ count }}</button>
+    <div class="text-main">
+      <p>
+        Últimas do blog      
+      </p>
+    </div>    
+  </div>
+
+  <p>    
+    <span class="text-info">Configura algumas dicas para tracionar sua carreira em Front-End</span><br>
+    <a href="" target="_blank">5 dicas para sua carreira profissional</a>    
+  </p>
+  <p>    
+    <span class="text-info">Configura algumas dicas para tracionar sua carreira em BackEnd</span><br>
+    <a href="" target="_blank">5 dicas para sua carreira profissional</a>    
+  </p>
+    <p>    
+    <span class="text-info">Configura algumas dicas para tracionar sua carreira em Arquitetura</span><br>
+    <a href="" target="_blank">5 dicas para sua carreira profissional</a>    
+  </p>
+  <p class="read-the-docs">Ver Tudo</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+.text-main{
+  font-weight: bold
+}
+.text-info {
+color: #000;
+font-weight: bold /* negrito no texto */
+}
+
+</style>
